297b661b0cfd605a66deac08bbe5a76440eed2c7
Paulien van Alst <paulien@openvalue.nl>
2019-12-12 18:46:16 +0100

    Initial commit

---
+++ src/main/java/com/barcoding/episode3annotations/Episode3AnnotationsApplication.java
@@ -0,0 +1,13 @@
+package com.barcoding.episode3annotations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Episode3AnnotationsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Episode3AnnotationsApplication.class, args);
+  }
+
+}
