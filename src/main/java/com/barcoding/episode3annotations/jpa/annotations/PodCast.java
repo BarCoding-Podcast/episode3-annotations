921f2a89971c44d7f52d9bdfcf02d452c103cd7f
arnout <arnoutrijp@hotmail.com>
2019-12-13 21:22:38 +0100

    Added persistence examples.

---
+++ src/main/java/com/barcoding/episode3annotations/jpa/annotations/PodCast.java
@@ -0,0 +1,11 @@
+package com.barcoding.episode3annotations.jpa.annotations;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class PodCast {
+    @Id
+    private long id;
+    private String name;
+}
---
+++ src/main/java/com/barcoding/episode3annotations/jpa/nonannotations/PodCast.java
@@ -0,0 +1,15 @@
+package com.barcoding.episode3annotations.jpa.nonannotations;
+
+public class PodCast {
+
+        private long id;
+        private String name;
+
+        public PodCast() {
+        }
+
+        public PodCast(long id, String name) {
+            this.id = id;
+            this.name = name;
+        }
+}
